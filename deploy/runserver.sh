284d4576918edc2db6c7a7dccff4dd954b21ed1d
leswing <lilleswing@gmail.com>
2018-06-27 12:01:51 -0400

    Use Gunicorn

--- deploy/runserver.sh
+++ deploy/runserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PATH=/home/leswing/anaconda3/envs/kudos/bin:/home/leswing/anaconda3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 cd src/kudos
-python manage.py runserver 0.0.0.0:8000
+gunicorn kudos.wsgi -b 0.0.0.0:8000
